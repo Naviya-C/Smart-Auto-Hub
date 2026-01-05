744626a94e82b7ddb5bf4a1e5337f4a8154b7fe9
Naviya-C <naviyac491@gmail.com>
2026-01-05 12:38:33 +0530

    edit schema of loader.py and initialize vector_store

--- ChatBot/app/rag/loader.py
+++ ChatBot/app/rag/loader.py
@@ -4,10 +4,19 @@ def load_cars():
     try:
         response = (
             supabase_client.table("Car")
-            .select("brand, model, price, year, mileage, specifications")
+            .select(
+                "id, " \
+                "brand, " \
+                "model, " \
+                "price, " \
+                "year, " \
+                "mileage"
+                )
             .execute()
         )
+
         return response.data
+    
     except Exception as e:
         raise RuntimeError(f"Error loading cars from Supabase: {e}")
-        
+         
